b7f9593beea2bd6d99ee57fd2b91bc787aa15382
iiiiins <83681280+iiiiins@users.noreply.github.com>
2025-01-11 00:42:30 +0000

    Delete src directory

--- src/services/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://ruykhdsevmwnptvxiwll.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1eWtoZHNldm13bnB0dnhpd2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0ODQyMjgsImV4cCI6MjA1MDA2MDIyOH0.Pvtw2J6uU4A_r0ntkUwriKvkm751YbQ4IWNqmYMAtTU";
-export const supabase = createClient(supabaseUrl, supabaseKey);
